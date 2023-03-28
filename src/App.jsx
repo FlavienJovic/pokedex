fc11b6e3d2024ca8b5e30ecc64f096487b6b744c
FlavienJovic <flavien.jovic@gmail.com>
2023-03-28 11:47:37 +0200

    react basics 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import PokemonCard from './components/PokemonCard'
 import './App.css'
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (<div>
-    <PokemonCard/>
+    <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc}/>
     </div>
     
   )
